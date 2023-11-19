c932c2dbabc3ca69325643f48d793ade0f49f6c3
taneeshaa <kalrataneesha@gmail.com>
2023-11-19 14:10:58 +0530

    Behavior Tree

---
+++ Assets/Actions/GoToTarget.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+using UnityEditor.Experimental.GraphView;
+
+[System.Serializable]
+public class GoToTarget : ActionNode
+{
+    public NodeProperty<GameObject> selfGameObject, targetObject;
+    private Transform myTransform, nextWavepointTransform;
+    private Vector3 direction;
+    private float speed = 2f;
+    protected override void OnStart() {
+        myTransform = selfGameObject.Value.transform;
+        nextWavepointTransform = targetObject.Value.transform;
+    }
+
+    protected override void OnStop() {
+    }
+
+    protected override State OnUpdate() {
+        if(nextWavepointTransform != null)
+        {
+            moveToTarget();
+        }
+        
+        return State.Success;
+    }
+
+    void moveToTarget()
+    {
+        direction = nextWavepointTransform.position - myTransform.position; 
+        myTransform.Translate(direction * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     TheKiwiCoder.BehaviourTreeInstance behaviourTreeInstance;
+    public GameObject waypointParent;
+    private int randomIndex;
     void Start()
     {
         behaviourTreeInstance = GetComponent<TheKiwiCoder.BehaviourTreeInstance>();
         behaviourTreeInstance.SetBlackboardValue("selfGameObject", gameObject);
         behaviourTreeInstance.SetBlackboardValue("playerGameObject", GameObject.FindGameObjectWithTag("Player"));
+        behaviourTreeInstance.SetBlackboardValue("waypointParent", waypointParent);
+        behaviourTreeInstance.SetBlackboardValue("myTransform", gameObject);
     }
 
     void Update()
