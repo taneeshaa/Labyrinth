8a9664ddca7917cfd0f2b7bdfc324bc784556a89
taneeshaa <kalrataneesha@gmail.com>
2023-12-28 14:13:07 +0530

    Database First Commit

--- Assets/InventorySystem/Inventory/Scripts/InventoryObject.cs
+++ Assets/InventorySystem/Inventory/Scripts/InventoryObject.cs
@@ -5,33 +5,32 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
 public class InventoryObject : ScriptableObject
 {
+    public ItemDatabaseObject database;
     public List<InventorySlot> Container = new List<InventorySlot>();
     public void AddItem(ItemObject _item, int _amount)
     {
-        bool hasItem = false;
         for(int i = 0; i < Container.Count; i++)
         {
             if (Container[i].item == _item)
             {
                 Container[i].AddAmount(_amount);
-                hasItem = true;
-                break;
+                return;
             }
         }
-        if(!hasItem)
-        {
-            Container.Add(new InventorySlot(_item, _amount));
-        }
+        Container.Add(new InventorySlot(database.GetId[_item], _item, _amount));
+        
     }
 }
 
 [System.Serializable]
 public class InventorySlot
 {
+    public int ID;
     public ItemObject item;
     public int amount;
-    public InventorySlot(ItemObject _item, int _amount)
+    public InventorySlot(int _id, ItemObject _item, int _amount)
     {
+        ID = _id;
         item = _item;
         amount = _amount;
     }
